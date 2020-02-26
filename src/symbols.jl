a37fa2cb5c98e4cd3f72c4b1099bba40cfe8b6e8
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-02-26 13:57:08 -0500

    remove backslash

--- src/symbols.jl
+++ src/symbols.jl
@@ -1,3 +1,3 @@
 # Here, we collect and reverse the REPL's latex autocompletion map.
 using REPL
-const latex_symbol_map = Dict((k => v[1] for (k, v) in REPL.REPLCompletions.latex_symbols))
+const latex_symbol_map = Dict((k[2:end] => v[1] for (k, v) in REPL.REPLCompletions.latex_symbols))
